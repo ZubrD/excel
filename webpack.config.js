d53f5e01c0292762ecdce3c5b4c2bc323619e766
Денис Зубрицкий <fort_zd@mail.ru>
2023-06-23 09:16:49 +0700

    Завершена настройка webpack

---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+module.exports = (env, argv) => {
+  const isProd = argv.mode === "production";
+  const isDev = !isProd;
+
+  console.log("isProd: ", isProd);
+  console.log("isDev: ", isDev);
+
+  const filename = (ext) =>
+    isProd ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`;
+
+  const plugins = () => {
+    const base = [
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, "src", "favicon.ico"),
+            to: path.resolve(__dirname, "dist"),
+          },
+        ],
+      }),
+      new MiniCssExtractPlugin({
+        filename: filename("css"),
+      }),
+    ];
+
+    // if (isDev) {                     // НЕ ЛЮБЛЮ ESLINT!!!!!!!!!!
+    //   base.push(new ESLintPlugin());
+    // }
+
+    return base
+  };
+
+  return {
+    // target: 'web',    // автоматическая перезагрузка devServer работает и без него
+    context: path.resolve(__dirname, "src"),
+    entry: {
+      main: ["@babel/polyfill", "./index.js"],
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: filename("js"),
+      clean: true,
+    },
+    resolve: {
+      extensions: [".js"],
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+        "@core": path.resolve(__dirname, "src", "core"),
+      },
+    },
+    devServer: {
+      port: "3000",
+      open: true,
+      // hot: true       // автоматическая перезагрузка devServer работает и без него
+      watchFiles: "./",
+    },
+    devtool: isDev ? "source-map" : false,
+    plugins: plugins(),
+
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
